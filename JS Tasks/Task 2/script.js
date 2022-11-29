6027a22829b019efee715679c0a6963dc6cdecc4
irinapad <109092681+irinapad@users.noreply.github.com>
2022-11-29 19:14:43 +0200

    Add files via upload

---
+++ JS Tasks/Task 2/script.js
@@ -0,0 +1,10 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+document.querySelector("#btn__element").addEventListener ("click", (event) => {
+    document.querySelector("#btn__state").innerHTML++;
+}
+)
